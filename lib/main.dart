e371526667a9c4b491b27a8dbb7013a566d69ac4
manh8 <phamthemanh16102001hp@gmail.com>
2023-02-22 09:58:22 +0700

    Hiển thị khách hàng, Sản phẩm mua bán

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import 'package:room_finder_flutter/store/AppStore.dart';
 import 'package:room_finder_flutter/utils/AppTheme.dart';
 import 'package:room_finder_flutter/utils/RFConstant.dart';
 import 'package:provider/provider.dart';
+import 'package:room_finder_flutter/providers/NhuCaus.dart';
 
 AppStore appStore = AppStore();
 
@@ -42,6 +43,16 @@ class MyApp extends StatelessWidget {
               auth.token,
               auth.userId,
               previousSanPhams == null ? [] : previousSanPhams.items,
+            );
+          },
+          // create: ,
+        ),ChangeNotifierProxyProvider<Auth, NhuCaus>(
+          create: (_) => NhuCaus('', '', []),
+          update: (_, auth, previousNhuCaus) {
+            return NhuCaus(
+              auth.token,
+              auth.userId,
+              previousNhuCaus == null ? [] : previousNhuCaus.items,
             );
           },
           // create: ,
--- lib/providers/SanPham.dart
+++ lib/providers/SanPham.dart
@@ -1,69 +1,32 @@
 import 'package:flutter/cupertino.dart';
 import 'package:nb_utils/nb_utils.dart';
-import 'package:room_finder_flutter/providers/KhachHangChuNha.dart';
-import 'package:room_finder_flutter/providers/KhuVuc.dart';
 
 class SanPham with ChangeNotifier {
   String? nid;
-  final String? title;
-  final String? field_dia_chi;
+  final String title;
+  final String field_dia_chi;
   final double? field_so_tang;
   final String? field_duong;
   final String? field_huong;
-  final double? field_gia;
-  final double? field_gia_bang_so;
+  final double field_gia;
+  final String? field_image;
   final String? field_don_vi_tinh;
-  final int? field_sale;
-  final String? field_phap_ly;
+  final int field_sale;
   final String? field_phan_loai_nhom_san_pham;
   final double? field_dien_tich;
-  final KhuVuc? field_phuong_xa;
-  final KhuVuc? field_quan_huyen;
-  final String? field_nhom_nhu_cau;
-  final String? field_ngay_nhap;
-  final String? field_ghi_chu;
-
-  // Thuộc tính sp cho thuê
-  final String? field_tinh_trang_noi_that;
-  final double? field_so_tien_coc;
-  final double? field_dien_tich_su_dung;
-  final double? field_chieu_dai;
-  final double? field_chieu_rong;
-  final int? field_so_phong_ve_sinh;
-  final int? field_so_phong_ngu;
-  late List<String>? field_anh_san_pham = [];
-
-  final KhachHangChuNha? khachHangChuNha;
 
   SanPham({
     this.nid = '',
-    this.title,
-    this.field_dia_chi,
+    required this.title,
+    required this.field_dia_chi,
     this.field_so_tang,
     this.field_duong,
     this.field_huong,
-    this.field_gia,
-    this.field_gia_bang_so,
+    required this.field_gia,
+    this.field_image,
+    required this.field_sale,
     this.field_don_vi_tinh,
-    this.field_sale,
-    this.field_phap_ly,
     this.field_phan_loai_nhom_san_pham,
-    this.field_dien_tich,
-    this.field_phuong_xa,
-    this.field_quan_huyen,
-    this.field_nhom_nhu_cau,
-    this.field_ngay_nhap,
-    this.field_tinh_trang_noi_that,
-    this.field_so_tien_coc,
-    this.field_dien_tich_su_dung,
-    this.field_chieu_dai,
-    this.field_chieu_rong,
-    this.field_so_phong_ve_sinh,
-    this.field_so_phong_ngu,
-    this.field_anh_san_pham,
-    this.khachHangChuNha,
-    this.field_ghi_chu
+    this.field_dien_tich
   });
-
-
 }
