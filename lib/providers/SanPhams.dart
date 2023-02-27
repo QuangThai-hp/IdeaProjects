734e54daddba422ba0ef4b41ed77dc88a17ba83d
hungddvimaru <hungddvimaru@gmail.com>
2023-02-27 23:34:20 +0700

    Fix nhóm nhu cầu

--- lib/providers/SanPhams.dart
+++ lib/providers/SanPhams.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
@@ -7,6 +8,7 @@ import 'package:room_finder_flutter/models/http_exeption.dart';
 import 'package:room_finder_flutter/providers/SanPham.dart';
 import 'package:room_finder_flutter/utils/RFString.dart';
 
+import '../screens/RFHomeScreen.dart';
 import 'SanPham.dart';
 
 class SanPhams with ChangeNotifier {
@@ -24,7 +26,7 @@ class SanPhams with ChangeNotifier {
 
   // Lấy danh sách sản phẩm theo loại (Mua bán / Cho thuê)
   Future<void> getListSanPham(String type) async{
-    // try
+    try
     {
       final response = await http.post(
           Uri.parse(RFGetSanPhamByType),
@@ -39,33 +41,82 @@ class SanPhams with ChangeNotifier {
           }
       );
 
-      print(jsonDecode(response.body));
-      final extractedData = List<Map<String, dynamic>>.from(jsonDecode(response.body)['content']); //json.decode(response.body) as Map<String, dynamic>;
-      final List<SanPham> loadedSanPhams = [];
-      extractedData.forEach((element) {
-        loadedSanPhams.add(SanPham(
-            nid: element['nid'],
-            title: element['title'],
-            field_dia_chi: element['field_dia_chi'],
-            field_so_tang: element['field_so_tang'].toString().toDouble(),
-            field_duong: element['field_duong'],
-            field_huong: element['field_huong'],
-            field_gia: element['field_gia'].toString().toDouble(),
-            field_image: element['field_image'],
-            field_don_vi_tinh: element['field_don_vi_tinh'],
-            field_sale: element['field_sale'].toString().toInt(),
-            field_phan_loai_nhom_san_pham: element['field_phan_loai_nhom_san_pham'],
-            field_dien_tich: element['field_dien_tich'].toString().toDouble(),
-        ));
-      });
-      _items = loadedSanPhams;
+      // final extractedData = List<Map<String, dynamic>>.from(jsonDecode(response.body)['content']); //json.decode(response.body) as Map<String, dynamic>;
+      // final List<SanPham> loadedSanPhams = [];
+      // extractedData.forEach((element) {
+      //   if(type == 'Cho thuê')
+      //     loadedSanPhams.add(SanPham(
+      //         nid: element['nid'],
+      //         title: element['title'],
+      //         field_dia_chi: element['field_dia_chi'],
+      //         field_so_tang: element['field_so_tang'].toString().toDouble(),
+      //         field_duong: element['field_duong'],
+      //         field_huong: element['field_huong'],
+      //         field_gia: element['field_gia'].toString().toDouble(),
+      //         field_don_vi_tinh: element['field_don_vi_tinh'],
+      //         field_sale: element['field_sale'].toString().toInt(),
+      //         // field_phan_loai_nhom_san_pham: element['field_phan_loai_nhom_san_pham'],
+      //         // field_dien_tich: element['field_dien_tich'].toString().toDouble(),
+      //     ));
+      //   else{
+      //     loadedSanPhams.add(SanPham(
+      //       nid: element['nid'],
+      //       title: element['title'],
+      //       field_dia_chi: element['field_dia_chi'],
+      //       field_so_tang: element['field_so_tang'].toString().toDouble(),
+      //       field_duong: element['field_duong'],
+      //       field_huong: element['field_huong'],
+      //       field_gia: element['field_gia'].toString().toDouble(),
+      //       field_don_vi_tinh: element['field_don_vi_tinh'],
+      //       field_sale: element['field_sale'].toString().toInt(),
+      //       // field_phan_loai_nhom_san_pham: element['field_phan_loai_nhom_san_pham'],
+      //       // field_dien_tich: element['field_dien_tich'].toString().toDouble(),
+      //     ));
+      //   }
+      // });//
+      // _items = loadedSanPhams;
       // if(!responseData['success'])
       //   throw HttpException(responseData['content']);
       notifyListeners();
     }
-    // catch(error){
-    //   throw error;
-    // }
+    catch(error){
+      throw error;
+    }
+  }
+
+  Future<void> save(Map<String, dynamic> sanPham, String routeAfterSave, BuildContext context) async {
+    try
+    {
+      final response = await http.post(
+          Uri.parse(RFSaveSanPham),
+          body: json.encode({
+            'uid': uid,
+            'auth': authToken,
+            'sanPham': sanPham
+          }),
+          headers: {
+            'Content-Type': 'application/json;charset=UTF-8',
+            'Charset': 'utf-8',
+          }
+      );
+      final responseData = json.decode(response.body);
+
+      print(responseData);
+
+      if(!responseData['success'])
+        throw HttpException(responseData['content']);
+      else{
+        RFHomeScreen rfHomeScreenFragment = new RFHomeScreen();
+        rfHomeScreenFragment.selectedIndex = 1;
+        rfHomeScreenFragment.contentAlert = responseData['content'];
+        rfHomeScreenFragment.showDialog = true;
+        rfHomeScreenFragment.launch(context);
+      }
+      notifyListeners();
+    }
+    catch(error){
+      throw error;
+    }
   }
 
 }
